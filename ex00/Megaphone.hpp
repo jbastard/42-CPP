b89724ac73c2c73b5ff24ee9e9b9b6df1123ad6d
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-04-08 10:13:34 +0200

    + | CPP0/-ex00 done

--- ex00/Megaphone.hpp
+++ ex00/Megaphone.hpp
@@ -2,5 +2,6 @@
 # define MEGAPHONE_HPP
 
 #include <iostream>
+#include <cstring>
 
 #endif
--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,12 @@
-
 #include "Megaphone.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-	std::cout << "Hello World !";
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char) toupper(av[i][j]);
+	std::cout << std::endl;
 	return (0);
 }
