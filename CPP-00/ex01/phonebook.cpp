e7231cd060f46c754c86a123690cf97ad17bae94
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-04-09 08:23:47 +0200

    ! | Main de test

--- CPP-00/ex01/phonebook.cpp
+++ CPP-00/ex01/phonebook.cpp
@@ -2,7 +2,13 @@
 #include "../includes/Contact.hpp"
 #include "../includes/Phonebook.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
-
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char) toupper(av[i][j]);
+	std::cout << std::endl;
+	return (0);
 }
