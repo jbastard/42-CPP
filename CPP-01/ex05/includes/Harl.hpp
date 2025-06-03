b3921560ca701927983bb1eb4f7531454e2668ad
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-06-03 15:22:06 +0200

    + | ex05, done

---
+++ CPP-01/ex05/includes/Harl.hpp
@@ -0,0 +1,39 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+# define ERR_ARGS "Wrong numbers of args, try with ./harl_2.0 <MSG>\nDEBUG - INFO - WARNING - ERROR\n"
+# define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!"
+# define INFO_MSG "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+# define WARNING_MSG "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month."
+# define ERROR_MSG "This is unacceptable! I want to speak to the manager now."
+
+
+#define RESET       "\033[0m"
+
+#define BOLD_BRIGHT_RED	"\033[1;91m"
+#define BRIGHT_ORANGE	"\033[1;93m"
+#define BRIGHT_GREEN    "\033[1;92m"
+#define BRIGHT_BLUE     "\033[1;94m"
+
+class Harl {
+
+private:
+
+	void	debug();
+	void	info();
+	void	warning();
+	void	error();
+
+public:
+
+	Harl();
+	~Harl();
+	void	complain(std::string level);
+
+};
+
+typedef void (Harl::*HarlFunction)();
+
+#endif
---
+++ CPP-01/ex05/srcs/Harl.cpp
@@ -0,0 +1,37 @@
+
+#include "../includes/Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void	Harl::debug()	{std::cout << BRIGHT_BLUE << DEBUG_MSG << RESET << std::endl;}
+void	Harl::info()	{std::cout << BRIGHT_GREEN << INFO_MSG << RESET << std::endl;}
+void	Harl::warning() {std::cout << BRIGHT_ORANGE << WARNING_MSG << RESET << std::endl;}
+void	Harl::error()	{std::cout << BOLD_BRIGHT_RED << ERROR_MSG << std::endl;}
+
+void	Harl::complain(std::string level) {
+
+	std::string		level_index[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	HarlFunction	function_tab[] = {
+			&Harl::debug,
+			&Harl::info,
+			&Harl::warning,
+			&Harl::error
+	};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == level_index[i])
+			(this->*function_tab[i])();
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (std::cout << ERR_ARGS, 0);
+	Harl harl;
+
+	harl.complain(av[1]);
+}
