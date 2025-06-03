9365f489202efe0074f72a761cea7b663e26b39b
Jonas Bastard <177691715+jbastard@users.noreply.github.com>
2025-06-03 15:47:54 +0200

    + | CPP01 done, verifs a faire

---
+++ CPP-01/ex06/srcs/main.cpp
@@ -0,0 +1,37 @@
+
+#include "../includes/Harl.hpp"
+
+int	get_level(std::string	level)
+{
+	int level_index = 0;
+	if (level == "DEBUG")
+		level_index = 1;
+	if (level == "INFO")
+		level_index = 2;
+	if (level == "WARNING")
+		level_index = 3;
+	if (level == "ERROR")
+		level_index = 4;
+	return (level_index);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (std::cout << ERR_ARGS, 0);
+
+	Harl harl;
+	int level = get_level((std::string)av[1]);
+	switch (level) {
+		case 1:
+			harl.complain("DEBUG");
+		case 2:
+			harl.complain("INFO");
+		case 3:
+			harl.complain("WARNING");
+		case 4:
+			harl.complain("ERROR");
+		default:
+			std::cout << UNEXPECTED_ARGS;
+	}
+}
